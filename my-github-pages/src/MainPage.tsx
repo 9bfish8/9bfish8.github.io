426b86153ebbb7549eaa3decd2df5e22fe8b015b
jeong-gayeon <1bfish106@naver.com>
2024-09-13 23:56:14 +0900

    Revert "feat/게시글 및 레이아웃 작업"
    This reverts commit 632de7d122d1f7009e7d03a90ea3b15f71da6384.
    
    # Conflicts:
    #	static/media/first.png

--- my-github-pages/src/MainPage.tsx
+++ my-github-pages/src/MainPage.tsx
@@ -1,18 +1,13 @@
-import React, { useState } from 'react';
-import { Box } from '@chakra-ui/react';
-import CustomTab from '../src/component/tab';
-import PostList from '../src/postitem';  // 게시글 목록 컴포넌트
+import UserProfileCard from "./component/profile";
+import React from "react";
 
-const MainPage: React.FC = () => {
-    const [activeTab, setActiveTab] = useState('홈');
-
+function MainPage() {
     return (
-        <Box>
-            <Box mt={6} ml="230px">
-                {(activeTab === '홈' || activeTab === '게시글') && <PostList />}
-            </Box>
-        </Box>
+
+        <>
+        </>
+
     );
-};
+}
 
 export default MainPage;
